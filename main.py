ea2ad0492ba2d5054ca722fb25727586440ae234
Jaidev Ashok <51042460+jaidevash@users.noreply.github.com>
2024-10-30 01:45:31 +0530

    Setting up initial project structure

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from typing import Any
+
+from common.constants import *
+from common.utils import *
+
+def run_model():
+    """
+    Main driver function for the model calculations
+
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+
+    # Load inputs
+
+    inputs = {'example' : 'example'}
+
+    run_model(**inputs)
