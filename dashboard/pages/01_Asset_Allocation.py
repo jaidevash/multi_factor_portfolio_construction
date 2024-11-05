cb759ad1a5d4640e884527d38489d33e0cb01082
Jaidev Ashok <51042460+jaidevash@users.noreply.github.com>
2024-11-05 10:20:11 +0530

    Added mroe visualization to Correlation Analysis page while streamlining Asset Allocation functionality

--- dashboard/pages/01_Asset_Allocation.py
+++ dashboard/pages/01_Asset_Allocation.py
@@ -3,6 +3,7 @@ import sys
 import streamlit as st
 import pandas as pd
 import sqlite3
+import plotly.express as px
 
 from common.constants import *
 from db.data_ingestion import ingest_ticker_data, rebalance_asset_weights, update_portfolio_and_weights
@@ -18,7 +19,7 @@ class DashboardAssetAlloc:
         self.tables = {}
         self.edited_tables = {}
         self.set_page_config()
-        self.expose_db()
+        self.show_all()
         st.divider()
 
     @staticmethod
@@ -29,71 +30,117 @@ class DashboardAssetAlloc:
             page_icon=":chart_with_upwards_trend:"
         )
 
-    def expose_db(self):
+    def show_all(self):
 
         with sqlite3.connect(self.db_path) as conn:
+            self.conn = conn
 
             st.title("Asset Allocation")
             st.write('#### Edits can be made directly to the tables below. ')
             st.divider()
 
-            cursor = conn.cursor()
-            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            tables = cursor.fetchall()
+            # Get a full list of tables from the DB, then read them into a dictionary (class attr)
+            self.extract_all_db_tables()
 
-            for table in tables:
-                table_name = table[0]
-                df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
-                self.tables[table_name] = df
+            # Split the page into 2 columns
+            cols = st.columns(2)
 
-            cols = st.columns(3)
-
+            # Column 1
             with cols[0]:
-                tbl = 'asset_allocation'
-                st.subheader(f"Table: {tbl}")
-                self.edited_tables[tbl] = st.data_editor(self.tables[tbl], num_rows='dynamic')
+                commit_required = self.expose_db()
 
-                # Warnings to display
-                if self.edited_tables[tbl].groupby('portfolio_id')['asset_weight'].sum().ne(1).any():
-                    st.warning("Portfolio weights require rebalancing!")
+                # If button clicked, commit edited tables to DB
+                if commit_required:
+                    self.commit_edited_tables_to_db()
 
-                assets_price_unavailable = list(
-                    set(self.edited_tables[tbl]['asset_id'].dropna().values)
-                    -
-                    set(self.tables['asset_prices']['asset_id'].values)
-                )
-
-                if len(assets_price_unavailable) > 0:
-                    st.warning(
-                        f"Database needs to be updated with prices of new assets: "
-                        f"{', '.join(assets_price_unavailable)}"
-                    )
-
-                tbl = 'portfolios'
-                st.subheader(f"Table: {tbl}")
-                self.edited_tables[tbl] = st.data_editor(self.tables[tbl], num_rows='dynamic')
-
-                tbl = 'assets'
-                st.subheader(f"Table: {tbl}")
-                st.write("_(asset_id refers to trading ticker)_")
-                self.edited_tables[tbl] = st.data_editor(self.tables[tbl], num_rows='dynamic')
-
-                clicked = st.button('Update database and rebalance weights', type='primary')
-
-                if clicked:
-                    for k, df in self.edited_tables.items():
-                        df.to_sql(k, conn, index=False, if_exists='replace')
-
-                    # Ingestion of new ticker data always the first thing to do
-                    ingest_ticker_data(self.edited_tables['asset_allocation']['asset_id'].values)
-                    update_portfolio_and_weights()
-
+            # Column 2
             with cols[1]:
-                self.show_corr_analysis()
+                self.show_portfolio_visualization()
 
-    def show_corr_analysis(self):
-        st.subheader('Index correlation analysis')
-        st.write("_(comparison with macro variables in the next section)_")
+    def extract_all_db_tables(self):
+
+        conn = self.conn
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+        for table in tables:
+            table_name = table[0]
+            df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+            self.tables[table_name] = df
+
+    def display_warnings(self):
+
+        # If weights in the asset_allocation table don't add up to 100% within a portfolio
+        if self.edited_tables['asset_allocation'].groupby('portfolio_id')['asset_weight'].sum().ne(1).any():
+            st.warning("Portfolio weights require rebalancing!")
+
+        assets_price_unavailable = list(
+            set(self.edited_tables['asset_allocation']['asset_id'].dropna().values)
+            -
+            set(self.tables['asset_prices']['asset_id'].values)
+        )
+
+        # If there are assets provided for which price data hasn't yet been loaded into the DB
+        if len(assets_price_unavailable) > 0:
+            st.warning(
+                f"Database needs to be updated with prices of new assets: "
+                f"{', '.join(assets_price_unavailable)}"
+            )
+
+    def expose_db(self):
+
+        # Show asset allocation table - make it editable
+        tbl = 'asset_allocation'
+        st.subheader(f"Table: {tbl} (editable)")
+        self.edited_tables[tbl] = st.data_editor(self.tables[tbl], num_rows='dynamic')
+
+        # Warnings to display - e.g. if portfolio weights need rescaling
+        self.display_warnings()
+
+        # Show asset allocation table - make it editable
+        tbl = 'portfolios'
+        st.subheader(f"Table: {tbl} (editable)")
+        self.edited_tables[tbl] = st.data_editor(self.tables[tbl], num_rows='dynamic')
+
+        button_msg = 'Update Database and Rebalance Weights'
+        button_clicked = st.button(button_msg, type='primary')
+
+        # Add a reference table for asset tickers and names
+        tbl = 'assets'
+        st.subheader(f"Reference table: {tbl}")
+        st.write("_(asset_id refers to trading ticker)_")
+        self.edited_tables[tbl] = st.data_editor(self.tables[tbl])
+
+        return button_clicked
+
+    def commit_edited_tables_to_db(self):
+        for k, df in self.edited_tables.items():
+            df.to_sql(k, self.conn, index=False, if_exists='replace')
+
+        # Ingestion of new ticker data is always the first thing to do
+        ingest_ticker_data(self.edited_tables['asset_allocation']['asset_id'].values)
+
+        # Update all table based on changes made mainly to asset_allocation
+        update_portfolio_and_weights()
+
+        # Refresh page
+        st.rerun()
+
+    def show_portfolio_visualization(self):
+        st.subheader('Portfolio overview')
+
+        df = self.tables['asset_allocation']
+        df_dict = {k: v for k, v in df.groupby('portfolio_id')}
+
+        for k, v in df_dict.items():
+            fig = px.pie(
+                v, values='asset_weight', names='asset_id',
+                title=k
+            )
+
+            st.plotly_chart(fig)
+
+        st.write("_(market trends and macro variables in the next section)_")
 
 
 DashboardAssetAlloc()
--- dashboard/pages/02_Correlation_Analysis.py
+++ dashboard/pages/02_Correlation_Analysis.py
@@ -1,20 +1,23 @@
 import os
 import sys
+import numpy as np
 import pandas as pd
 import streamlit as st
 import sqlite3
 import plotly.express as px
 
 from common.constants import *
+from calculations.calc_portfolio import calc_portfolio_price, calc_portfolio_returns
 
 class DashboardCorrelation:
 
     def __init__(self) -> None:
         self.db_path = f"db/{DB_NAME}"
         self.tables = {}
+        self.pf_id_selected = None
 
         self.set_page_config()
-        self.show_rolling_correlations()
+        self.show_all()
 
     @staticmethod
     def set_page_config() -> None:
@@ -24,36 +27,83 @@ class DashboardCorrelation:
             page_icon=":chart_with_upwards_trend:"
         )
 
-    def show_rolling_correlations(self):
+    def show_all(self):
 
         st.title("Correlation Analysis")
+        st.write('#### Select a portfolio, then flip through the tabs for analyses. ')
 
         with sqlite3.connect(self.db_path) as conn:
+            self.conn = conn
+            self.add_portfolio_dropdown()
 
-            pf_ids = pd.read_sql_query("SELECT DISTINCT portfolio_id FROM portfolios", conn)
-            pf_ids = pf_ids.iloc[:, 0].values
+            # Spit dashboard page into tabs
+            tabs = st.tabs(['Market Trends', 'Macro-economic Indicators'])
 
-            cols = st.columns(4)
+            with tabs[0]:
+                self.show_market_trends()
 
-            with cols[0]:
-                pf_id = st.selectbox("Select portfolio whose returns you wish to analyze", pf_ids)
 
-            cols = st.columns(2)
 
-            with cols[0]:
-                assets_tbl =  pd.read_sql_query((
-                    f"SELECT asset_allocation.asset_id, asset_allocation.asset_weight, asset_prices.date, asset_prices.asset_price "
-                    f"FROM asset_allocation JOIN asset_prices ON asset_allocation.asset_id = asset_prices.asset_id "
-                    f"WHERE asset_allocation.portfolio_id = '{pf_id}'"
-                ), conn)
+    def add_portfolio_dropdown(self):
 
-                # st.dataframe(assets_tbl)
-                #
-                fig = px.line(assets_tbl, x='date', y='asset_price', color='asset_id', title='Index Price Trends',
-                    # color_discrete_sequence='yellow'
-                )
-                fig.show()
+        conn = self.conn
+        pf_ids = pd.read_sql_query("SELECT DISTINCT portfolio_id FROM portfolios", conn)
+        pf_ids = pf_ids.iloc[:, 0].values
 
-            # asset_prices =
+        cols = st.columns(4)  # This is only to control dropdown size
+        with cols[0]:
+            self.pf_id_selected = st.selectbox("Pick the portfolio whose returns are to be analyzed", pf_ids)
+
+    def show_market_trends(self):
+
+        conn = self.conn
+
+        assets_tbl = pd.read_sql_query((
+            f"SELECT asset_allocation.portfolio_id, asset_allocation.asset_id, asset_allocation.asset_weight, asset_prices.date, asset_prices.asset_price "
+            f"FROM asset_allocation JOIN asset_prices ON asset_allocation.asset_id = asset_prices.asset_id "
+            f"WHERE asset_allocation.portfolio_id = '{self.pf_id_selected}'"
+        ), conn)
+
+        # Chart 1
+        fig = px.line(
+            assets_tbl, x='date', y='asset_price',
+            color='asset_id',
+            title='Individual Index Price Trends (log scale; non-homogeneous currencies)',
+            # color_discrete_sequence='yellow',
+            log_y=True,
+        )
+        st.plotly_chart(fig)
+
+        # Table to support chart 1
+        assets_tbl_pivot = assets_tbl.pivot(index='date', columns='asset_id', values='asset_price').reset_index()
+        st.dataframe(assets_tbl_pivot)
+
+        # Calcs to support chart 2
+        assets_tbl = calc_portfolio_price(assets_tbl)
+        pf_tbl = calc_portfolio_returns(assets_tbl)
+
+        # Chart 2
+        fig = px.line(
+            pf_tbl, x='date', y='price',
+            title='Portfolio Price Trends (log scale)',
+            # color_discrete_sequence='yellow',
+            log_y=True,
+        )
+        st.plotly_chart(fig)
+        st.caption('Price is weight-averaged based on weights provided')
+
+        # Chart 3
+        fig = px.line(
+            pf_tbl, x='date', y='log_return',
+            title='Portfolio Log Returns (period-over-period)',
+            # color_discrete_sequence='yellow',
+            # log_y=True,
+        )
+        st.plotly_chart(fig)
+
+        # Table to support charts 2 and 3
+        st.dataframe(pf_tbl)
+
+
 
 DashboardCorrelation()
