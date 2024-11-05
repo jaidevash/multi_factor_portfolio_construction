1003eed4248e7693f2c3934186b3aad5885ab6a0
Jaidev Ashok <51042460+jaidevash@users.noreply.github.com>
2024-11-05 14:06:42 +0530

    No longer neededl integrated in page 2

--- dashboard/pages/03_Return_Attribution.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import streamlit as st
-
-st.set_page_config(
-    layout="wide",
-    page_title="Dashboard - by Jaidev Ashok",
-    page_icon=":chart_with_upwards_trend:"
-)
