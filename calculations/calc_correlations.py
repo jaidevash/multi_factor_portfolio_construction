16497f5807ec87a7e76727b98b92b4aa943061bb
Jaidev Ashok <51042460+jaidevash@users.noreply.github.com>
2024-11-05 14:07:18 +0530

    Updated calculations; switched from Lasso to Ridge as Lasso was too penalizing

--- calculations/calc_correlations.py
+++ calculations/calc_correlations.py
@@ -1,4 +1,23 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LassoCV
+from sklearn.linear_model import RidgeCV
+
+def calc_primary_coefficients(X, y):
+    """
+    Gets highest contributing variables using L2 regularization
+    """
+
+    print(X.isna().sum().sum())
+    print(X.shape)
+    print(y.shape)
+
+    model = RidgeCV()
+    model.fit(X.values, y.values)
+
+    coefficients = pd.DataFrame({'Feature': X.columns, 'Coefficient': model.coef_})
+
+    print(model.coef_.sum())
+    print(model.coef_)
+
+    return coefficients
 
--- calculations/calc_portfolio.py
+++ calculations/calc_portfolio.py
@@ -8,7 +8,18 @@ def calc_portfolio_price(assets_tbl):
     :return:
     """
 
-    assets_tbl['weighted_price'] = assets_tbl['asset_weight'] * assets_tbl['asset_price']
+    assets_tbl_pivot = assets_tbl.pivot(index=['portfolio_id', 'date', 'asset_weight'], columns='asset_id', values='asset_price')
+    # assets_tbl['weighted_price'] = assets_tbl['asset_weight'] * assets_tbl['asset_price']
+    asset_pct_return = assets_tbl_pivot.pct_change().melt(ignore_index=False, var_name='asset_id', value_name='asset_pct_return')
+    asset_log_return = np.log(assets_tbl_pivot / assets_tbl_pivot.shift(1)).melt(ignore_index=False, var_name='asset_id', value_name='asset_log_return')
+
+    asset_pct_return = asset_pct_return.set_index('asset_id', append=True)
+    asset_log_return = asset_log_return.set_index('asset_id', append=True)
+
+    assets_tbl = pd.concat([asset_pct_return, asset_log_return], ignore_index=False, axis=1).reset_index()
+
+    assets_tbl['weighted_pct_return'] = assets_tbl['asset_weight'] * assets_tbl['asset_pct_return']
+    assets_tbl['weighted_log_return'] = assets_tbl['asset_weight'] * assets_tbl['asset_log_return']
 
     return assets_tbl
 
@@ -20,8 +31,10 @@ def calc_portfolio_returns(assets_tbl):
     :return:
     """
 
-    pf_tbl = assets_tbl.groupby(['portfolio_id', 'date'])['weighted_price'].sum().rename('price').reset_index()
-    pf_tbl['pct_return'] = pf_tbl['price'].pct_change()
-    pf_tbl['log_return'] = np.log(pf_tbl['price'] / pf_tbl['price'].shift(1))
+    pf_tbl = assets_tbl.groupby(['portfolio_id', 'date'])[['weighted_pct_return', 'weighted_log_return']].sum().reset_index()
+
+    pf_tbl = pf_tbl.rename(columns={'weighted_pct_return' : 'pct_return', 'weighted_log_return': 'log_return'})
+    # pf_tbl['pct_return'] = pf_tbl['price'].pct_change()
+    # pf_tbl['log_return'] = np.log(pf_tbl['price'] / pf_tbl['price'].shift(1))
 
     return pf_tbl
